6c98830d0abcf7567ce9c9227551c637f5245961
szbr1 <szb8480@gmail.com>
2025-03-26 17:37:41 +0500

    New To Arrive

--- ecommerce-webite/frontend/app/components/Layouts/Scroller.jsx
+++ ecommerce-webite/frontend/app/components/Layouts/Scroller.jsx
@@ -42,7 +42,6 @@ function Scroller() {
   ];
   // All the states const are defined here
   const controlRef = useRef(null);
-  const [canScroll, setCanScroll] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
@@ -50,31 +49,41 @@ function Scroller() {
   const [canScrollLeft, setCanScrollLeft] = useState(false);
 
   // this segment ends here
+  const hanldeMouseUp = () => {
+    setIsDragging(false)
+    document.removeEventListener("mousemove", handleMouseDragg);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
+
 
   const handleMouseDown = (e) => {
-    setStartX = e.pageX - controlRef.current.offsetLeft;
-    setIsDragging = true;
-    setCanScroll: controlRef.current.scrollLeft;
+    setIsDragging (true)
+    setStartX( e.pageX - controlRef.current.offsetLeft)
+    setScrollLeft(controlRef.current.scrollLeft)
+
+    // Add event listener dynamically
+  document.addEventListener("mousemove", handleMouseDragg);
+  document.addEventListener("mouseup", hanldeMouseUp )
 
   };
 
 
   const handleMouseDragg = (e) => {
-    if(!isDragging){
-    const x = e.pageX - controlRef.current.offseLeft;
+    if (!isDragging) return;
+    e.preventDefault(); 
+    const x = e.pageX - controlRef.current.offsetLeft;
     const walk = x - startX;
-    controlRef.current.scrollLeft = scrollLeft - walk
-    }
+    controlRef.current.scrollLeft = scrollLeft - walk;
   };
+  
 
-  const hanldeMouseUp = () => {};
 
   const handleMouseLeave = () => {};
 
   // Left Right Image Scroller Buttons
   const scroll = (direction) => {
-    const scrollleft = direction === "left" ? -500 : 500;
-    controlRef.current.scrollBy({ left: scrollleft, behaviour: "smooth" });
+    const scrollleft = direction === "left" ? -373 : 373;
+    controlRef.current.scrollBy({ left: scrollleft, behavior : "smooth" });
   };
 
   const updateScrollbuttons = () => {
@@ -106,10 +115,7 @@ function Scroller() {
     <div className=" relative">
       <div
         className="absolute lg:right-20 top-3 gap-5 flex z-10 right-6  "
-        onMouseDown={handleMouseDown}
-        onMouseUp={hanldeMouseUp}
-        onMouseLeave={handleMouseLeave}
-        onMouseMove={handleMouseDragg}
+       
       >
         <button
           className="border p-2 border-black rounded-full cursor-pointer disabled:text-gray-400 disabled:border-gray-300  "
@@ -127,23 +133,31 @@ function Scroller() {
         </button>
       </div>
       <br />
+ {/* scrollabe container  */}
 
       <div
         ref={controlRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={hanldeMouseUp}
+        onMouseLeave={handleMouseLeave}
+        onMouseMove={handleMouseDragg}
         className="container  relative mx-auto overflow-x-scroll h-64-900 gap-3 flex "
       >
         {products.map((product) => {
           return (
             <div
-              className="mt-8 h-full w-full mx-2 rounded-md md:w-1/2 lg:w-64 flex-shrink-0 overflow-hidden"
+              className="mt-8 h-full w-full px-2  md:w-1/2 lg:w-64 flex-shrink-0 overflow-hidden rounded-lg relative"
               key={product.id}
             >
               <img
                 src={product.url}
-                className="object-cover h-full w-full"
+                className="object-cover h-full w-full  rounded-lg"
                 alt=""
                 draggable="false"
               />
+             <div className="absolute bottom-0 text-white backdrop-blur-sm w-full p-2">
+<div>{product.title}</div>
+             </div>
             </div>
           );
         })}
