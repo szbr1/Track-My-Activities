849e4402971b22c6e0f13cf2d4b3c7dd5afa24f7
szbr1 <szb8480@gmail.com>
2025-06-14 18:56:44 +0500

    docs: buffers ..

--- Work-Files/node-docs/script.js
+++ Work-Files/node-docs/script.js
@@ -2,14 +2,53 @@
 
 
 //! EventEmitter 
-const EventEmitter  = require('events')
+// const EventEmitter  = require('events')
 
-class Emiter extends EventEmitter {}
+// class Emiter extends EventEmitter {}
 
-const emiter = new Emiter()
+// const emiter = new Emiter()
 
-emiter.on('karlo',()=>{
-    console.log("My name is shahziab")
-})
+// emiter.on('karlo',()=>{
+//     console.log("My name is shahziab")
+// })
 
-emiter.emit('karlo')
+// emiter.emit('karlo')
+
+//! Buffers 
+
+// const {Buffer} = require("buffer")
+
+// //lenghty way
+// //*const bf = Buffer.alloc(3)
+// // bf[0] = 0x48
+// // bf[1] = 0x69
+// // bf[2] = 0x21
+
+
+// //short way 
+// //* const bf = Buffer.from([0x48, 0x69, 0x21])
+
+// //shorter way 
+// //* const bf = Buffer.from ("486921", "hex")
+
+
+// // console.log(bf.toString('utf-8'))
+
+
+// //shortest way 
+// const bf = Buffer.from("Hello", "utf-8")
+// console.log(bf) // this will be the hex data in of this
+// console.log(bf.toString("utf-8"))
+
+
+//!how buffers shows on cpu with taskbar
+
+// const {Buffer} = require("buffer")
+
+// const buff = Buffer.alloc(0.5e9)
+
+// setInterval(()=>{
+//     for(let i = 0; i < buff.length; i++){
+//      buff[i] = 0x22
+//     }
+// })
