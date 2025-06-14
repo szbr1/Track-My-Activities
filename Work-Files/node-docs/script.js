44aacdc8a68b7c338ad7d45511b4fae5c554c21b
szbr1 <szb8480@gmail.com>
2025-06-14 08:05:14 +0500

    docs:Learned complete funtionality of EventEmitter

--- Work-Files/node-docs/script.js
+++ Work-Files/node-docs/script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+//? Day 2 : 14/06/2025
+
+
+//! EventEmitter 
+const EventEmitter  = require('events')
+
+class Emiter extends EventEmitter {}
+
+const emiter = new Emiter()
+
+emiter.on('karlo',()=>{
+    console.log("My name is shahziab")
+})
+
+emiter.emit('karlo')
