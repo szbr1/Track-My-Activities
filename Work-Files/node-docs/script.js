ee8e255908bf0b2dd3f73f6e11b656f563e4f2be
szbr1 <szb8480@gmail.com>
2025-06-15 18:24:31 +0500

    docs: hello

--- Work-Files/node-docs/script.js
+++ Work-Files/node-docs/script.js
@@ -53,12 +53,185 @@
 //     }
 // })
 
+//// ! File System    
+
+// const fs = require("fs")
+
+// fs.writeFileSync("file.txt",'my namd is ')
+
+// const read = fs.readFileSync("file.txt")
+
+// console.log(read.toString('utf-8'))
+
+ //_____________________________________________________________________________________________________
+ ////!fs wathc 
+// _____________________________________________________________________________________________________
+
+// const fs = require("fs/promises");
 
 
-const fs = require("fs")
+// (async () => {
+//     try {
+//       await  fs.copyFile("file.txt", "updated.jsx")
+//         console.log("File created Successfully")
+//     } catch (error) {
+//         console.error(error)
+//     }
+//     })();
 
-fs.writeFileSync("file.txt",'my namd is ')
+//_______)))))))___________)))__________)________)))))__________________________________________________________
 
-const read = fs.readFileSync("file.txt")
+// //! fs watcher 
 
-console.log(read.toString('utf-8'))
+//  const fs = require("fs/promises");
+
+
+// (async() => {
+
+//    try {
+//       const watcher = fs.watch("./")
+
+//       for await(const event of watcher){
+//           if(event.eventType ===  'change' && event.filename === "file.txt"){
+//             console.log("file changes occurs")
+//           }
+//       }
+
+//    } catch (error) {
+//     console.error(error)
+//    }
+// })()
+
+
+
+
+//----------------------------------------------------
+// const fs = require("fs/promises");
+
+// (async () => {
+//   const commandFileHandler = await fs.open("./command.txt", "r");
+
+//   commandFileHandler.on("change", async () => {
+//     // get the size of our file
+//     const size = (await commandFileHandler.stat()).size;
+//     // allocate our buffer with the size of the file
+//     const buff = Buffer.alloc(size);
+//     // the location at which we want to start filling our buffer
+//     const offset = 0;
+//     // how many bytes we want to read
+//     const length = buff.byteLength;
+//     // the position that we want to start reading the file from
+//     const position = 0;
+
+//     // we always want to read the whole content (from beginning all the way to the end)
+//     const content = await commandFileHandler.read(
+//       buff,
+//       offset,
+//       length,
+//       position
+//     );
+
+//     console.log(content);
+//   });
+
+//   // watcher...
+//   const watcher = fs.watch("./command.txt");
+//   for await (const event of watcher) {
+//     if (event.eventType === "change") {
+//       commandFileHandler.emit("change");
+//     }
+//   }
+// })();
+
+
+
+//! How to create file with node on any path 
+
+const fs = require("fs/promises");
+
+// const text = "create new file";
+  
+
+(async () => {
+    
+   ////! create file fc
+    // const createFile = async( path )=>{
+    //    if(!path) return;
+    //    try {
+    //    const filereading = await fs.open(path , 'r')
+    //    filereading.close()
+    //     console.log("this file exist already path:",path)
+
+    //    } catch (error) {
+    //     const fileCreate  = await fs.open(path, "w")
+    //     console.log("file created")
+    //     fileCreate.close()
+    //    }
+    // }
+
+    // const renameFile = async(oldPath, newPath)=>{
+    //     if(!oldPath || !newPath) return;
+    //     await fs.rename(oldPath, newPath)
+    //     console.log("success")
+    // }
+  ////! add text to the file fc
+    // const editFile  = async (path, content)=>{
+    //     if(!path) return;
+    //     await fs.writeFile(path, content)
+    //     console.log("success true")
+    // }
+
+  //! remove file fc
+    const removeFile = async(path)=>{
+    if(!path) return;
+    await fs.unlink(path)
+    }
+    
+    
+  try {
+    // const rFile = "rename the file"
+    // const edit = "edit the file"
+    const remove = "remove the file"
+
+    const commandFile = await fs.readFile("command.txt", "utf8"); 
+
+
+//     console.log(commandFile.includes(text)) // true or false
+// // this substring will give the starting position 
+//     const path = commandFile.substring(text.length + 1 )
+//     createFile(path)
+ 
+     //! renaem the file
+    // if(commandFile.includes(rFile)){
+    //     const idx = commandFile.indexOf(" to ")
+    //     const oldPath = commandFile.substring(rFile.length + 1 , idx)
+    //     const newPath = commandFile.substring(idx + 4)
+    //     renameFile(oldPath, newPath)
+
+    // }
+
+//    //! to add text to the file 
+//    if(commandFile.includes(edit)){
+//     const idx = commandFile.indexOf(" content: ")
+//     const pathName = commandFile.substring(edit.length + 1 , idx)
+//     const content = commandFile.substring(idx + 10)
+//     console.log("pathName", pathName  ," content: ", content)
+//     editFile(pathName,content)
+//    }
+
+//! to delete the file 
+    if(commandFile.includes(remove)){
+       const path = commandFile.substring(remove.length + 1)
+       removeFile(path)
+    }
+     
+    
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
+})();
+
+
+
+
+
