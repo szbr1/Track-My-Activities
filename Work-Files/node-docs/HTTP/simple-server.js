0ac03efc8ae760abf9592f932d47f5a6dc676126
szbr1 <szb8480@gmail.com>
2025-06-23 07:06:22 +0500

    docs: Delete some files and Create first http server

--- Work-Files/node-docs/HTTP/simple-server.js
+++ Work-Files/node-docs/HTTP/simple-server.js
@@ -1,40 +1,19 @@
 const http = require("node:http");
+const fs = require("node:fs/promises");
 
-const server = http.createServer();
+const server = http.createServer()
 
-server.on("request", (request, response) => {
-  console.log("--------- METHOD: ---------");
-  console.log(request.method);
+server.on("request",async(request,response)=>{
 
-  console.log("--------- URL: ---------");
-  console.log(request.url);
+  if(request.url === "/" && request.method === "GET"){
+    response.setHeader("Content-Type", "text/html")
+    const filehandle = await fs.open("./public/index.html", "r")
+    const fileStream = filehandle.createReadStream()
+    
+    fileStream.pipe(response)
+  }
+})
 
-  console.log("--------- HEADERS: ---------");
-  console.log(request.headers);
-
-  const name = request.headers.name;
-
-  console.log("--------- BODY: ---------");
-
-  let data = "";
-
-  request.on("data", (chunk) => {
-    data += chunk.toString();
-    console.log(data);
-  });
-
-  request.on("end", () => {
-    data = JSON.parse(data);
-
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(
-      JSON.stringify({
-        message: `Post with title ${data.title} was created by ${name}`,
-      })
-    );
-  });
-});
-
-server.listen(8050, () => {
-  console.log("Server listening on http://localhost:8050");
-});
+server.listen(8000,()=>{
+  console.log("Server is running on http://localhost:8000")
+})
