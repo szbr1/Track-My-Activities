72a434334e71cc5c09be84a872b8ae400341eabf
szbr1 <szb8480@gmail.com>
2025-06-29 05:26:21 +0500

    Fix wsl errors / clusters [28/06/2025]

---
+++ Work-Files/node-docs/unix/clustering/script.js
@@ -0,0 +1,14 @@
+const cluster = require('node:cluster');
+
+if (cluster.isPrimary) {
+  const worker = cluster.fork();
+  worker.on('exit', (code, signal) => {
+    if (signal) {
+      console.log(`worker was killed by signal: ${signal}`);
+    } else if (code !== 0) {
+      console.log(`worker exited with error code: ${code}`);
+    } else {
+      console.log('worker success!');
+    }
+  });
+}
