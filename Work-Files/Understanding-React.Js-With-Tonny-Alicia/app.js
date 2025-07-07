44c4a00efd3d7cde2b34ea8ae377339fce602f92
szbr1 <szb8480@gmail.com>
2025-07-07 05:39:51 +0500

    last day missed commit: react under-hood source code and understanding

--- Work-Files/Understanding-React.Js-With-Tonny-Alicia/app.js
+++ Work-Files/Understanding-React.Js-With-Tonny-Alicia/app.js
@@ -1,1 +1,23 @@
-console.log(React)
+// Get the root DOM node where the React app will be mounted
+const rootElement = document.getElementById('root');
+
+// Define a simple React functional component
+function App() {
+    return React.createElement(
+        'div',
+        { className: 'text-2xl font-bold text-blue-600' },
+        'Hello from React!'
+    );
+}
+
+// Use ReactDOM to render the App component into the root element
+const root = ReactDOM.createRoot(rootElement);
+root.render(React.createElement(App, null));
+
+// Now you can log them:
+console.log('The DOM element:', rootElement);
+console.log('The React root object:', root);
+
+const Hello = ()=>{
+    console.log(rootElement)
+}
