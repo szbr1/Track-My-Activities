7c3e27af9ea45a97005427e315359ac624fba78f
szbr1 <szb8480@gmail.com>
2025-08-14 11:23:32 +0500

    backend main file updated

--- chat-application/server/src/app.js
+++ chat-application/server/src/app.js
@@ -7,25 +7,17 @@ import cors from "cors";
 import AuthRoutes from "./routes/api.js";
 import MsgRoutes from "./routes/msg.api.js";
 import monogoDB from "./lib/db.js";
-import { server, io, app as socketApp } from "./lib/socketio.js";
+
+import { server,app } from "./lib/socketio.js";
 
 dotenv.config();
 
-const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev, dir: "./frontend" });
-const handle = nextApp.getRequestHandler();
 
-nextApp.prepare().then(() => {
-  const app = socketApp; // use the same app instance from socketio.js
 
   app.use(cookieParser());
   app.use(express.json());
   app.use(cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps, curl)
-      if (!origin) return callback(null, true);
-      return callback(null, origin);
-    },
+    origin: "https://track-my-activities.vercel.app",
     credentials: true
   }));
 
@@ -36,11 +28,10 @@ nextApp.prepare().then(() => {
   // DB connection
   monogoDB();
 
-  // Let Next.js handle everything else
-  app.all("*", (req, res) => handle(req, res));
+
 
   const PORT = process.env.PORT || 8000;
   server.listen(PORT, () => {
     console.log(`Server ready on http://localhost:${PORT}`);
   });
-});
+
--- chat-application/server/src/lib/socketio.js
+++ chat-application/server/src/lib/socketio.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: true,
+    origin: "https://track-my-activities.vercel.app",
     credentials: true,
   },
 });
