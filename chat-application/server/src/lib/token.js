00fa927aa5ae7886535cf0f4150fe806b2765f36
Shahzaib Mirza <szb8480@gmail.com>
2025-08-14 16:16:59 +0500

    fix: force token to work

--- chat-application/server/src/lib/token.js
+++ chat-application/server/src/lib/token.js
@@ -1,22 +1,24 @@
- import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
- const jsonGenerate = (id, res)=>{
-     try {
-        // here i created the token 
+const jsonGenerate = (id, res) => {
+    try {
+        const token = jwt.sign(
+            { userId: id },
+            process.env.TOKEN,
+            { expiresIn: "7d" }
+        );
 
-        const token = jwt.sign({userId: id} ,process.env.TOKEN, {expiresIn: "7d"})
-
-
-       // here i store that into the the cookies
-            res.cookie('token', token, {
-                maxAge: 7 * 24 * 60 * 60 * 1000,
-                httpOnly: true,
-                secure: true,
-                sameSite: "none"
-            }) 
+        res.cookie('token', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            domain: ".track-my-activities.vercel.app", // 👈 force cookie to stick to your frontend domain
+            path: "/" // 👈 available on the whole site, not just /api
+        });
     } catch (error) {
-        return res.status(500).json('Entery fail')
+        return res.status(500).json('Entry fail');
     }
- }
+};
 
- export default jsonGenerate;
+export default jsonGenerate;
