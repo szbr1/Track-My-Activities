18ce8a139381e6b015c393d43068d0abae6bb29a
szbr1 <szb8480@gmail.com>
2025-09-17 00:59:47 +0500

    message config complete

---
+++ projects in chunks/chat-app/src/routes/chat.js
@@ -0,0 +1,48 @@
+import express, { response } from "express"
+import User from "../controllers/user.model.js";
+import Message from "../controllers/chat.model.js";
+
+const route = express.Router()
+
+
+route.post("/send-message", async(req,res)=>{
+    try {
+        const {message, senderName, recieverName} = req.body;
+          if(!message || !senderName || !recieverName){
+            return res.status(401).json({message: "You Can't Send Message"})
+          }
+         
+        const msg = Message.create({
+            message,
+            recieverName,
+            senderName
+        }) 
+
+        return res.status(200).json({message: msg})
+
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({message: "Server is not wokring"})
+    }
+})
+
+
+route.get("/fetch-messages", async(req,res)=>{
+    try {
+       const {senderName, recieverName} = req.body;
+
+      const messages = await Message.find({$or: [{senderName, recieverName},{recieverName, senderName}]})
+
+      return res.status(200).json({message: messages})
+
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({message: "Server is not wokring"})
+    }
+})
+
+
+export default route;
+
+
+
